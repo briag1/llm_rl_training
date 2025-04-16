76f70c0efc0288b9a7d62553b9c28fe135747965
briag1 <briag.rehel@hotmail.fr>
2025-04-16 23:55:31 +0200

    feat: Add device.

--- src/generation/generator.py
+++ src/generation/generator.py
@@ -11,6 +11,8 @@ from abc import ABC, abstractmethod
 class GeneratorConfig:
     model_name: str = Field(default= "HuggingFaceTB/SmolLM2-135M-Instruct")
     max_new_token: int = Field(default = 100)
+
+
     
 class Generator(ABC):
     def __init__(self, generator_config : Optional[GeneratorConfig] = None):
@@ -19,6 +21,10 @@ class Generator(ABC):
         self.model = LlamaForCausalLM.from_pretrained(self.generator_config.model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(self.generator_config.model_name)
         self.max_new_token  = self.generator_config.max_new_token
+    
+    def to(self, device: str)->None:
+        self.model.to(device)
+        self.device = device
         
     @abstractmethod
     def generate(self, query: str) -> GeneratorOutput:
--- src/generation/greedy_generator.py
+++ src/generation/greedy_generator.py
@@ -12,12 +12,15 @@ class GreedyGenerator(Generator):
         len_initial_inputs_ids = inputs_ids.shape[1]
         
         while True:
+            inputs_ids.to(self.device)
             model_out = self.model.forward(inputs_ids)
             probs = torch.nn.functional.softmax(model_out.logits, dim=-1)
             next_token_id = torch.multinomial(probs[:, -1, :], num_samples=1)  # Sampling top token
             inputs_ids = torch.cat([inputs_ids, next_token_id], dim=-1)
 
             if next_token_id == 2 or inputs_ids.shape[1] - len_initial_inputs_ids >= self.max_new_token:
+                inputs_ids = inputs_ids.cpu()
+                probs= probs.cpu()
                 return GeneratorOutput(
                     sequence = self.tokenizer.decode(inputs_ids.squeeze()[len_initial_inputs_ids:]), 
                     probabilites = probs.squeeze()[len_initial_inputs_ids-1:], 
