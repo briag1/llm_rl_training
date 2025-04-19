6b8cd897def51398e858f5941883d7c666d8c812
briag1 <briag.rehel@hotmail.fr>
2025-04-19 13:44:25 +0200

    fix: Reward devide by 0.

--- src/environements/rewarder.py
+++ src/environements/rewarder.py
@@ -11,7 +11,7 @@ class Rewarder:
             parsed_output = self.parser.parse(generator_out)
             reward += 1
             result = float(parsed_output.result)
-            reward += 1/abs(result-generator_out) #/abs(result+generator_out)
+            reward += 1/abs(result-generator_out)+1e-6 #/abs(result+generator_out)
         except:
             return reward - 0.01
 
